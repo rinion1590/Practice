fe969289fb2823aa326d03822f92bc8e5e213dd7
{rinion1590} <rinion1590@gmail.com>
2023-05-21 23:30:52 +0900

    first commit

---
+++ Practice/Java/Basic1/B1.java
@@ -0,0 +1,16 @@
+package Practice.Java.Basic1;
+
+public class B1 {
+    public static void main(String[] args){
+        //変数 HelloWorldを代入
+        String Hello = "HelloWorld";
+        //変数HelloとHiをくっつけてprint
+        System.out.println("Hi "+Hello);
+        //変数numに3を代入
+        int num =  3;
+        System.out.println(num);
+        //numをインクリメント
+        num++;
+        System.out.println(num);
+}
+}
---
+++ Practice/Java/HelloWorld/HelloWorld.java
@@ -0,0 +1,7 @@
+package Practice.Java.HelloWorld;
+
+class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, world.");
+	}
+}
