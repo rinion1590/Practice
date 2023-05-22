1c6dd41770b30b7f2287c24b395437e757f2456a
{rinion1590} <rinion1590@gmail.com>
2023-05-22 12:54:24 +0900

    modified B2.java to fix the error

--- Practice/Java/Basic2/B2.java
+++ Practice/Java/Basic2/B2.java
@@ -4,14 +4,16 @@ import java.util.Scanner;
 public class B2 {
     public static void main(String[] args){
         System.out.println("numの値を入力...");
-        Scanner scan = new Scanner(System.in);
-        int num = scan.nextInt();
-        //numの2で割った時の商が0=偶数
-        if (num%2==0){
-            System.out.println(num+"は偶数です。");
-        //違う場合は奇数
-        }else{
-            System.out.println(num+"は奇数です。");
+        //try挟んだらエラー消えたから気にしない。
+        try (Scanner scan = new Scanner(System.in)) {
+            int num = scan.nextInt();
+            //numの2で割った時の商が0=偶数
+            if (num%2==0){
+                System.out.println(num+"は偶数です。");
+            //違う場合は奇数
+            }else{
+                System.out.println(num+"は奇数です。");
+            }
         }
     }
 }
