bef298db7adc12c16349824d67e11c6849d29bf4
{rinion1590} <rinion1590@gmail.com>
2023-05-22 12:46:59 +0900

    Modefied B2.java

--- Practice/Java/Basic2/B2.java
+++ Practice/Java/Basic2/B2.java
@@ -1,8 +1,11 @@
 package Practice.Java.Basic2;
+import java.util.Scanner;
 
 public class B2 {
     public static void main(String[] args){
-        int num = 0;
+        System.out.println("numの値を入力...");
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
         //numの2で割った時の商が0=偶数
         if (num%2==0){
             System.out.println(num+"は偶数です。");
