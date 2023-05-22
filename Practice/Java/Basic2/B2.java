c09b7416f5a55b3abda004f71b5b3095ca2e42b9
{rinion1590} <rinion1590@gmail.com>
2023-05-22 12:18:16 +0900

    Create B2.java

---
+++ Practice/Java/Basic2/B2.java
@@ -0,0 +1,14 @@
+package Practice.Java.Basic2;
+
+public class B2 {
+    public static void main(String[] args){
+        int num = 0;
+        //numの2で割った時の商が0=偶数
+        if (num%2==0){
+            System.out.println(num+"は偶数です。");
+        //違う場合は奇数
+        }else{
+            System.out.println(num+"は奇数です。");
+        }
+    }
+}
